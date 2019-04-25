80047f31f04b5e690b3e8542029fe089beb54794
Sam Thiriot <samuel.thiriot@res-ear.ch>
2019-04-25 15:00:31 +0200

    do a copy of files when the file is on a network share (fix #3 fix #2)

--- ch.res_ear.samthiriot.knime.dbase/src/ch/res_ear/samthiriot/knime/dbase/readDromDBase/DBaseReaderNodeModel.java
+++ ch.res_ear.samthiriot.knime.dbase/src/ch/res_ear/samthiriot/knime/dbase/readDromDBase/DBaseReaderNodeModel.java
@@ -2,11 +2,15 @@ package ch.res_ear.samthiriot.knime.dbase.readDromDBase;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.InvalidPathException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
@@ -36,6 +40,8 @@ import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.util.CheckUtils;
+import org.knime.core.util.FileUtil;
 
 import nl.knaw.dans.common.dbflib.CorruptedTableException;
 import nl.knaw.dans.common.dbflib.DbfLibException;
@@ -229,14 +235,49 @@ public class DBaseReaderNodeModel extends NodeModel {
     	final String filename = m_config.getLocation();
     	if (filename == null)
 	    	throw new InvalidSettingsException("No filename provided");
-	    	
- 
+
     	logger.debug("opening DBaser file "+filename);
 
-        File file = new File(filename);
-        if (!file.canRead())
-	    	throw new InvalidSettingsException("Cannot read file "+file);
+    	// check this path can be accessed
+        CheckUtils.checkSourceFile(filename);
 
+        // convert to an url, so we accept path relative to workflows
+        URL url;
+		try {
+			url = FileUtil.toURL(filename);
+		} catch (InvalidPathException | MalformedURLException e2) {
+			e2.printStackTrace();
+			throw new InvalidSettingsException("unable to open URL "+filename+": "+e2.getMessage());
+		}
+        
+		File file = null;
+		if (FileUtil.isWindowsNetworkMount(url)) {
+			
+			// there are errors on network mount if the file is open by 
+			// several programs. 
+			// create a local tmp copy, its easier and better.=
+			logger.debug("doing a local copy of the file to avoid windows share issues");
+			try {
+				file = FileUtil.createTempFile("dbase_", ".dbf", true);
+				
+				FileUtils.copyInputStreamToFile(
+						url.openStream(), 
+						file);
+				
+			} catch (IOException e) {
+
+				throw new RuntimeException("error when reading the DBase file "+filename+": "+e.getMessage());
+			}
+			
+
+		} else {
+			// just read this file.
+			file = new File(filename);
+	        if (!file.canRead())
+		    	throw new InvalidSettingsException("Cannot read file "+file);
+
+		}
+		
         // open the table
         Table table = new Table(file);
 		try {
@@ -248,7 +289,7 @@ public class DBaseReaderNodeModel extends NodeModel {
 			e.printStackTrace();
 			throw new IllegalArgumentException("error reading data from database "+filename, e);
 		} 
-		
+
 		return table;
 		
     }
@@ -260,67 +301,79 @@ public class DBaseReaderNodeModel extends NodeModel {
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
             final ExecutionContext exec) throws Exception {
 
-        // open the table
-    	Table table = getDBaseTable();
 
-    	// read the fields from the table...
-	    final List<Field> fields = table.getFields();
-	    
-	    // will create the data columns in KNIME based on the specs from the DBase file 
-	    DataColumnSpec[] allColSpecs = getKnimeDataColSpecsForDBaseFields(fields);
-	 
-        // the data table spec of the single output table, 
-        DataTableSpec outputSpec = new DataTableSpec(allColSpecs);
-        
-        // the execution context will provide us with storage capacity, in this
-        // case a data container to which we will add rows sequentially
-        // Note, this container can also handle arbitrary big data tables, it
-        // will buffer to disc if necessary.
-        BufferedDataContainer container = exec.createDataContainer(outputSpec);
-        
-        
-        final boolean readDeleted = m_config.getReadDeletedRows();
-        final boolean trimStrings = m_config.getTrimStrings();
-        
-        Iterator<Record> itRecords = table.recordIterator(readDeleted);
-        
-        int maxLines = m_config.getLimitRowsCount();
-        if (maxLines < 0) {
-        	maxLines = table.getRecordCount();
-        	logger.debug("will read all the "+maxLines+" lines");
-        } else {
-        	logger.info("will only read the "+maxLines+" first lines (as defined by settings)");
-        }
-        int currentLine = 0;
-        
-        
-        while (itRecords.hasNext() && (currentLine < maxLines)) {
-        	Record currentRecord = itRecords.next();
-        	
-        	container.addRowToTable(
-        			new DefaultRow(
-	        			new RowKey("Row " + currentLine), 
-	        			decodeDBFields(fields, currentRecord, exec, trimStrings)
-	        			)
-        			);
-            
-        	currentLine++;
-        	
-        	if (currentLine % 10 == 0) { 
-	            // check if the execution monitor was canceled
-	            exec.checkCanceled();
-	            exec.setProgress(
-	            		(double)currentLine / maxLines, 
-	            		"Adding row " + currentLine);
-        	}
-        }
-        
-        pushFlowVariableInt("count", maxLines);
-        
-        // once we are done, we close the container and return its table
-        container.close();
-        BufferedDataTable out = container.getTable();
-        return new BufferedDataTable[]{out};
+    	Table table = null;
+    	try {
+	        // open the table
+	    	table = getDBaseTable();
+	
+	    	// read the fields from the table...
+		    final List<Field> fields = table.getFields();
+		    
+		    // will create the data columns in KNIME based on the specs from the DBase file 
+		    DataColumnSpec[] allColSpecs = getKnimeDataColSpecsForDBaseFields(fields);
+		 
+	        // the data table spec of the single output table, 
+	        DataTableSpec outputSpec = new DataTableSpec(allColSpecs);
+	        
+	        // the execution context will provide us with storage capacity, in this
+	        // case a data container to which we will add rows sequentially
+	        // Note, this container can also handle arbitrary big data tables, it
+	        // will buffer to disc if necessary.
+	        BufferedDataContainer container = exec.createDataContainer(outputSpec);
+	        
+	        
+	        final boolean readDeleted = m_config.getReadDeletedRows();
+	        final boolean trimStrings = m_config.getTrimStrings();
+	        
+	        Iterator<Record> itRecords = table.recordIterator(readDeleted);
+	        
+	        int maxLines = m_config.getLimitRowsCount();
+	        if (maxLines < 0) {
+	        	maxLines = table.getRecordCount();
+	        	logger.debug("will read all the "+maxLines+" lines");
+	        } else {
+	        	logger.info("will only read the "+maxLines+" first lines (as defined by settings)");
+	        }
+	        int currentLine = 0;
+	        
+	        
+	        while (itRecords.hasNext() && (currentLine < maxLines)) {
+	        	Record currentRecord = itRecords.next();
+	        	
+	        	container.addRowToTable(
+	        			new DefaultRow(
+		        			new RowKey("Row " + currentLine), 
+		        			decodeDBFields(fields, currentRecord, exec, trimStrings)
+		        			)
+	        			);
+	            
+	        	currentLine++;
+	        	
+	        	if (currentLine % 10 == 0) { 
+		            // check if the execution monitor was canceled
+		            exec.checkCanceled();
+		            exec.setProgress(
+		            		(double)currentLine / maxLines, 
+		            		"Adding row " + currentLine);
+	        	}
+	        }
+	        
+	        pushFlowVariableInt("count", maxLines);
+	        
+	        // once we are done, we close the container and return its table
+	        container.close();
+	        BufferedDataTable out = container.getTable();
+	        return new BufferedDataTable[]{out};
+	        
+    	} finally {
+    		if (table != null)
+				try {
+					table.close();
+				} catch (IOException e) {
+					logger.warn("error when closing the dbase file "+ m_config.getLocation()+": "+e.getMessage());
+				}
+    	}
     }
 
     /**
